9fb71281a2d5c4ee04b363d39ff4189f05a05d68
Jonas Neugebauer <jonas@neugebauer.cc>
2020-06-01 21:20:25 +0200

    Changed width of textfields

--- tools/gettysetty/web.py
+++ tools/gettysetty/web.py
@@ -10,7 +10,7 @@ bp = Blueprint('gettysetty', __name__, template_folder='templates')
 def start():
 	if request.method == 'POST':
 		schema = request.form.get('schema', '')
-		clazz  = request.form.get('class', 'Klasse')
+		clazz  = request.form.get('class', 'GettySetty')
 
 		code = gen_class(clazz, parse_simple(schema))
 
