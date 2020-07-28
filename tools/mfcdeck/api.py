7c38077dd612fd3c063224e56a337b1c9a84b6da
Jonas Neugebauer <jneug@users.noreply.github.com>
2020-07-28 09:46:53 +0200

    Foxed result Format

--- tools/mfcdeck/api.py
+++ tools/mfcdeck/api.py
@@ -1,11 +1,9 @@
-from flask import Blueprint, request, current_app, g, render_template, url_for
-
-import requests
+from flask import Blueprint
 
 bp = Blueprint('mfcdeck.api', __name__, template_folder='templates')
 
 @bp.route('/calendar')
-def start():
+def calendar():
 	card = {
 		'mfcdeck': True,
 		'version': 2,
@@ -15,6 +13,6 @@ def start():
 	for day in range(31):
 		row.append(day)
 		if day%7 == 6:
-			card['items'].append(row)
+			card['items'].append({'items':row})
 			row = []
 	return card
