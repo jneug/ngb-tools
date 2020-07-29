19d432051f85010bd83499ceb93608ff4b91a70c
Jonas Neugebauer <jneug@users.noreply.github.com>
2020-07-29 19:42:27 +0200

    [mfcdeck] fixed yet another missing type in doc

--- tools/mfcdeck/web.py
+++ tools/mfcdeck/web.py
@@ -11,7 +11,7 @@ def start():
 			('background','Background gradient for the cards. Set to a comma separated list of hex colors.', 'str', '#4a6d88,#001e38'),
 			('color', 'Text color as hex color.', 'str', '#ffffff'),
 			('today_color', 'Color highlight for the current day as hex color.', 'str', '#fb00fc'),
-			('wend_color', 'Color highlight for weekends as hex color.', '#fec44c'),
+			('wend_color', 'Color highlight for weekends as hex color.', 'str', '#fec44c'),
 			('month_color', 'Color for the month name as hex color.', 'str', '#59b3f2'),
 			('wday_color', 'Color for the weekday names as hex color.', 'str', '#888892'),
 			('fs_small', 'Fontsize for small cards', 'int', 10),
