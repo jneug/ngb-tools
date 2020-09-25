eef7a22dbbf315710f465fb95461a5831c74f570
Jonas Neugebauer <jneug@users.noreply.github.com>
2020-09-25 16:08:58 +0200

    XMLRPC forwarder to fix a bug in Drafts.app

---
+++ tools/xmlrpc/__init__.py
@@ -0,0 +1,4 @@
+from . import api
+
+name = 'XML-RPC'
+description = ''
---
+++ tools/xmlrpc/api.py
@@ -0,0 +1,25 @@
+import requests
+from flask import Blueprint, request, Response
+
+import html
+
+bp = Blueprint('xmlrpc.api', __name__, template_folder='templates')
+
+@bp.route('/forward')
+def forward():
+	to = request.args.get('to', type=str)
+	
+	resp = requests.request(
+		method=request.method,
+		url=request.url.replace(request.host_url, to),
+		headers={key: value for (key, value) in request.headers if key != 'Host'},
+		data=request.get_data().replace('&amp;lt;', '&lt;'),
+		cookies=request.cookies,
+		allow_redirects=False
+	)
+
+	excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
+	headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
+
+	response = Response(resp.content, resp.status_code, headers)
+	return response
