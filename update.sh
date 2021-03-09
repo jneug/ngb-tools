cf999b724e82459947187a30ea8eb638fa3f014c
Jonas Neugebauer <jonas@neugebauer.cc>
2021-03-09 10:03:51 +0100

    update script update

--- update.sh
+++ update.sh
@@ -1,4 +1,6 @@
 source venv/bin/activate
 git pull
-pip install --ignore-installed -r requirements.txt
+venv/bin/pip install --upgrade pip
+venv/bin/pip install --ignore-installed -r requirements.txt
 supervisorctl restart tools
+deactivate
